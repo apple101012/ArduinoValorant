82e596876d3679e1aaa01c68f92d49cd20303bbf
Shihab Jamal <85631620+apple101012@users.noreply.github.com>
2025-02-27 15:37:21 -0500

    Fixed clicking

--- combinemousemoveandidtest/combinemousemoveandidtest.ino
+++ combinemousemoveandidtest/combinemousemoveandidtest.ino
@@ -8,6 +8,7 @@ USB Usb;
 HIDBoot<USB_HID_PROTOCOL_MOUSE> HidMouse(&Usb);
 
 int8_t dx = 0, dy = 0;  // Mouse movement variables
+int lmb = 0, rmb = 0, mmb = 0, xb1 = 0, xb2 = 0;  // Mouse button states
 
 class MouseParser : public MouseReportParser {
   protected:
@@ -15,6 +16,21 @@ class MouseParser : public MouseReportParser {
       dx = mi->dX;
       dy = mi->dY;
     }
+
+    void OnLeftButtonDown(MOUSEINFO *mi) override { lmb = 1; }
+    void OnLeftButtonUp(MOUSEINFO *mi) override { lmb = 0; }
+
+    void OnRightButtonDown(MOUSEINFO *mi) override { rmb = 1; }
+    void OnRightButtonUp(MOUSEINFO *mi) override { rmb = 0; }
+
+    void OnMiddleButtonDown(MOUSEINFO *mi) override { mmb = 1; }
+    void OnMiddleButtonUp(MOUSEINFO *mi) override { mmb = 0; }
+
+    void OnXB1ButtonDown(MOUSEINFO *mi) override { xb1 = 1; }
+    void OnXB1ButtonUp(MOUSEINFO *mi) override { xb1 = 0; }
+
+    void OnXB2ButtonDown(MOUSEINFO *mi) override { xb2 = 1; }
+    void OnXB2ButtonUp(MOUSEINFO *mi) override { xb2 = 0; }
 };
 
 MouseParser mouseParser;
@@ -35,13 +51,24 @@ void loop() {
     RawHID.read();
     x = rawHidData[0];
     y = rawHidData[1];
+    lmb = rawHidData[2] & 1;
+    rmb = (rawHidData[2] >> 1) & 1;
+    mmb = (rawHidData[2] >> 2) & 1;
+    xb1 = (rawHidData[2] >> 3) & 1;
+    xb2 = (rawHidData[2] >> 4) & 1;
     RawHID.enable();  // Flush data
   } else {
     x = dx;  // Use USB mouse input
     y = dy;
   }
 
+  // Move mouse
   Mouse.move(x, y);
-  dx = 0;  // Reset values after moving
+  dx = 0;
   dy = 0;
+
+  // Handle button presses
+  if (lmb == 0) { Mouse.release(MOUSE_LEFT); } else { Mouse.press(MOUSE_LEFT); }
+  if (rmb == 0) { Mouse.release(MOUSE_RIGHT); } else { Mouse.press(MOUSE_RIGHT); }
+  if (mmb == 0) { Mouse.release(MOUSE_MIDDLE); } else { Mouse.press(MOUSE_MIDDLE); }
 }
